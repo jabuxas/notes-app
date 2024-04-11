327e6a2836a721b3f672a8d9b436adb5cd7a496f
Lucas Barbieri <lucasbarbieri.c@gmail.com>
2024-04-11 18:40:44 -0300

    is it this?

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "notes-app",
+  base: "/notes-app/",
   plugins: [vue()],
   resolve: {
     alias: {
